fcbd975e075b039f490782cbd5ec8decc20f0be1
R0CKSTAR <yeahdongcn@gmail.com>
2013-12-11 13:20:56 +0800

    Add kvo for UITableView itself, imageview, textLabel, detailTextLabel's frames.

--- RSTransitionEffect/RSCell.m
+++ RSTransitionEffect/RSCell.m
@@ -8,13 +8,76 @@
 
 #import "RSCell.h"
 
+@interface RSCell ()
+
+@property (nonatomic, assign) CGRect cellFrame;
+
+@property (nonatomic, assign) CGRect cellImageViewFrame;
+
+@property (nonatomic, assign) CGRect cellTextLabelFrame;
+
+@property (nonatomic, assign) CGRect cellDetailTextLabelFrame;
+
+- (void)__init;
+
+@end
+
 @implementation RSCell
+
+#pragma mark - Private
+
+- (void)__init
+{
+    [self addObserver:self forKeyPath:@"frame" options:(NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld) context:NULL];
+    
+    [self.imageView addObserver:self forKeyPath:@"frame" options:(NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld) context:NULL];
+    
+    [self.textLabel addObserver:self forKeyPath:@"frame" options:(NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld) context:NULL];
+    
+    [self.detailTextLabel addObserver:self forKeyPath:@"frame" options:(NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld) context:NULL];
+}
+
+#pragma mark - NSObject
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        // Initialization code
+        [self __init];
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [self removeObserver:self forKeyPath:@"frame"];
+    [self.imageView removeObserver:self forKeyPath:@"frame"];
+    [self.textLabel removeObserver:self forKeyPath:@"frame"];
+    [self.detailTextLabel removeObserver:self forKeyPath:@"frame"];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+    if (object == self && [keyPath isEqualToString:@"frame"]) {
+        self.cellFrame = self.frame;
+    } else if (object == self.imageView && [keyPath isEqualToString:@"frame"]) {
+        self.cellImageViewFrame = self.imageView.frame;
+    } else if (object == self.textLabel && [keyPath isEqualToString:@"frame"]) {
+        self.cellTextLabelFrame = self.textLabel.frame;
+    } else if (object == self.detailTextLabel && [keyPath isEqualToString:@"frame"]) {
+        self.cellDetailTextLabelFrame = self.detailTextLabel.frame;
+    }
+}
+
+#pragma mark - UITableViewCell
 
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
         // Initialization code
+        [self __init];
     }
     return self;
 }
